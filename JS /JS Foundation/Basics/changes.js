b349511730a3486a4a65b2febd4a07a824d2a117
ashuu021 <ashuu023@gmail.com>
2025-08-22 15:28:31 +0530

    other concepts

---
+++ JS /JS Foundation/Basics/changes.js
@@ -0,0 +1,7 @@
+let gameName = "anshu021"
+gameName = "anshuu001"
+console.log(gameName);
+const userName = "anshu021"
+// userName = "anshu001" This throw an error.Const cant be redefined.
+console.log(userName);
+
