8800adae009c08b466e2d9ca4fe15d2d0a876dac
ashuu021 <ashuu023@gmail.com>
2025-08-22 15:42:37 +0530

    basic operations in js

---
+++ JS /JS Foundation/Basics/operation.js
@@ -0,0 +1,31 @@
+//Operation in JavaScript
+
+//Addition
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log("Sum:", sum);
+
+//Subtraction
+let difference = b - a;
+console.log("Difference:", difference);
+
+//Multiplication
+let product = a * b;
+console.log("Product:", product);
+
+//Division
+let quotient = b / a;
+console.log("Quotient:", quotient);
+
+//Assignment operator
+let c = 15;
+c += 5; // c = c + 5
+console.log("New value of c:", c);
+
+//Increment and Decrement operators
+let d = 10;
+d++; // Increment
+console.log("After incrementing d:", d);
+d--; // Decrement
+console.log("After decrementing d:", d);
