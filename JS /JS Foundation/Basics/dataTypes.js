1d516d1711e5cbd676829735f9f9b9155f1acd3b
ashuu021 <ashuu023@gmail.com>
2025-08-22 15:25:09 +0530

    dataTypes of js

---
+++ JS /JS Foundation/Basics/dataTypes.js
@@ -0,0 +1,10 @@
+console.log("Data types");
+var score =1; //Old type to declare a variable
+let rollNumber =102; //new Type //Int
+let name = "Anshu"; //String
+let isLoggeedin=false;//boolean
+let list=["name","rollNumber","age"]; //list
+
+console.log(score);
+console.log(name);
+
