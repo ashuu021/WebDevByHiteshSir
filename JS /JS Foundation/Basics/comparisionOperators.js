46afb4cca5abb187fec574466c311defecd03ca0
ashuu021 <ashuu023@gmail.com>
2025-08-22 15:43:54 +0530

    comparision operators

---
+++ JS /JS Foundation/Basics/comparisionOperators.js
@@ -0,0 +1,10 @@
+// Comparison Operators
+let x = 5;
+let y = 10;
+
+console.log("x == y:", x == y); // Equal to
+console.log("x != y:", x != y); // Not equal to
+console.log("x > y:", x > y);   // Greater than
+console.log("x < y:", x < y);   // Less than
+console.log("x >= y:", x >= y); // Greater than or equal to
+console.log("x <= y:", x <= y); // Less than or equal to
