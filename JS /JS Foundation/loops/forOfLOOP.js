4f1f7248629eac1b2628db7892abaa75eddc2cc6
ashuu021 <ashuu023@gmail.com>
2025-08-24 19:46:08 +0530

    use of for of loop

---
+++ JS /JS Foundation/loops/forOfLOOP.js
@@ -0,0 +1,13 @@
+let nameList =["anshu","pari","anshika","abhi"];
+let checkName = [];
+for (const name of nameList) {
+     if (name==="anshika") {
+        break;
+     }
+    checkName.push(name)
+}
+console.log(checkName);
+
+// for (const element of object) {
+    
+// }
