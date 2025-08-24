bfa67464509490314c611af0a50aac3c466a1fda
ashuu021 <ashuu023@gmail.com>
2025-08-24 18:50:41 +0530

    for loop

---
+++ JS /JS Foundation/loops/forloop.js
@@ -0,0 +1,8 @@
+// multiply a array with 2 and store it in another array
+
+let multiplyArray =[];
+let array = [1,2,3,4];
+for(let i =0;i<array.length;i++){
+    multiplyArray.push(array[i]*2);
+}
+console.log(multiplyArray);
