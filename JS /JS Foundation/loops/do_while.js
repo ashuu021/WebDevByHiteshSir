6f733710e0f5833e9ce4bd7d34e9cc50b9ff8af1
ashuu021 <ashuu023@gmail.com>
2025-08-24 18:37:20 +0530

    do while looop

---
+++ JS /JS Foundation/loops/do_while.js
@@ -0,0 +1,11 @@
+// add numbers from 1 to 3 and store the total in a variable 
+let total =0;
+let i =1;
+
+do {
+    // total = total+i;
+    total +=i;
+    i++;
+} while (i<=3);
+
+console.log(total);
