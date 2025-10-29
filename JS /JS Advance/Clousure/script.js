e96a5f303f8849230a0ee52486febf58046c7d55
ashuu021 <ashuu023@gmail.com>
2025-10-30 00:13:06 +0530

    Implement closure function to maintain counter state and log incremented values

--- JS /JS Advance/Clousure/script.js
+++ JS /JS Advance/Clousure/script.js
@@ -1,3 +1,12 @@
 function outer() {
-    
+    let counter =2;
+   return function () {
+    counter++;
+    return counter;
+   } ;
 }
+
+let count=outer();
+console.log(count());
+console.log(count());
+console.log(count());
