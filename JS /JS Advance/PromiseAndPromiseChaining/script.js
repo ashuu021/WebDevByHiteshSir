ab7fe96ecfc27e9598a63f87392d4cdddaf22ff0
ashuu021 <ashuu023@gmail.com>
2025-10-30 00:37:42 +0530

    Implement fetchData function with Promise and error handling

--- JS /JS Advance/PromiseAndPromiseChaining/script.js
+++ JS /JS Advance/PromiseAndPromiseChaining/script.js
@@ -1,3 +1,23 @@
-function name(params) {
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let success=true;
+            if (success) {
+                resolve("Data is fetched successfully.")
+            }else{
+                reject("Error in fetching data.")
+            }
+        }, 3000);
+
+    })
+};
+fetchData()
+.then((data)=>{
+    console.log(data);
     
-}
+    return data.toLowerCase();})
+.then((value)=>console.log(value)
+)
+.catch((error)=>console.log(error)
+)
+
