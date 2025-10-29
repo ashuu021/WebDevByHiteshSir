afd92b8eeafd986f3908961357e2333aa5f63e10
ashuu021 <ashuu023@gmail.com>
2025-10-29 19:58:46 +0530

    Refactor event listeners and update example comments in script.js

--- JS /JS BOM and DOM/part2/script.js
+++ JS /JS BOM and DOM/part2/script.js
@@ -35,10 +35,34 @@ document.getElementById("removeLastTask").addEventListener("click",function () {
  taskList.firstElementChild.remove();
 //  taskList.lastElementChild.remove();
 });
-//Example 6
-document.getElementById("replaceImage").addEventListener("click",function () {
-    let img=document.getElementById("myImage");
-    img.src="https://www.w3schools.com/html/pic_trulli.jpg";
-    img.alt="Trulli";
-    img.style.width="300px";
+
+// Example 6
+document.getElementById("clickMeButton").addEventListener("click",function () {
+    alert("Hello")
+    
+});
+
+//Example 7
+ document.getElementById("teaList").addEventListener("click",function (event) {
+    if(event.target ){
+        alert("You selected the "+event.target.textContent)
+    }
+ })
+// Example 8
+document.getElementById("feedbackForm").addEventListener("submit",function (event) {
+    event.preventDefault();
+    let feedback =document.getElementById("feedbackInput").value;
+    console.log(feedback);
+    document.getElementById("feedbackDisplay").textContent=`Feedback is ${feedback}`.toUpperCase()
+});
+//Example 9 
+ document.addEventListener("DOMContentLoaded",function () {
+    document
+    .getElementById("domStatus").textContent="Dom is loaded..";
+
+ });
+//  Example 10
+document.getElementById("toggleHighlight").addEventListener("click",function () {
+    let discriptionText=document.getElementById("descriptionText");
+    discriptionText.classList.toggle("highlight")
 });
